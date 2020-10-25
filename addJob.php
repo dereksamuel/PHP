4d3d71f957e82c7596ff4d7de79365141917d53c
Luis Fonseca <luis.fonseca@brilliant360.net>
2020-10-24 19:20:24 -0500

    En casa de Andy

---
+++ addJob.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./index.css">
+  <title>AddJob - User Management</title>
+</head>
+<body>
+  <main class="Hero">
+    <form action="" method="post">
+      <label for="title">
+        Title:
+        <input type="text" id="title" name="title">
+      </label>
+      <label for="desc">
+        Description:
+        <input type="text" name="desc" id="desc">
+      </label>
+      <label for="months">
+        Months:
+        <input type="text" id="months" name="months">
+      </label>
+      <input type="submit" value="ACCEPT">
+    </form>
+  </main>
+</body>
+</html>
